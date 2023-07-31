0e7366ac0d9e5076008751ede52b1d5f84b7a601
Antti <antti.linno@gmail.com>
2023-07-31 18:48:28 +0000

    Fix testutils and e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -26,39 +26,34 @@ describe('AppController (e2e)', () => {
   it('/todos (POST)', async () => {
     const result = (await request(app.getHttpServer()).post('/todos').send({ title: 'My first todo', content: 'First todo description' }));
 
-    console.log(result.body);
-
-    return true;
+    expect(result.body).toMatchObject({ title: 'My first todo', content: 'First todo description' });
   });
 
-  it.only('/todos (GET)', async () => {
+  it('/todos (GET)', async () => {
     const todo = await testUtils.createTodo({ title: 'Test todo', content: 'Long description of test todo' });
-
     const result = await request(app.getHttpServer()).get('/todos');
+
     expect(result.body).toContainEqual(todo);
   });
 
   it('/todos/:id (GET)', async () => {
-    const result = await request(app.getHttpServer()).get('/todos/7');
+    const todo = await testUtils.createTodo({ title: 'Test todo', content: 'Long description of test todo' });
+    const result = await request(app.getHttpServer()).get(`/todos/${todo.id}`);
 
-    console.log(result.body);
-
-    return true;
+    expect(result.body).toEqual(todo);
   });
 
   it('/todos/:id (PATCH)', async () => {
-    const result = (await request(app.getHttpServer()).patch('/todos/7').send({ title: 'My first patched todo', content: 'First todo patched description' }));
+    const todo = await testUtils.createTodo({ title: 'Test todo', content: 'Long description of test todo' });
+    const result = (await request(app.getHttpServer()).patch(`/todos/${todo.id}`).send({ title: 'My first patched todo', content: 'First todo patched description' }));
 
-    console.log(result.body);
-
-    return true;
+    expect(result.body).toEqual({ id: todo.id, title: 'My first patched todo', content: 'First todo patched description' });
   });
 
   it('/todos/:id (DELETE)', async () => {
-    const result = (await request(app.getHttpServer()).delete('/todos/7'));
+    const todo = await testUtils.createTodo({ title: 'Test todo', content: 'Long description of test todo' });
+    const result = (await request(app.getHttpServer()).delete(`/todos/${todo.id}`));
 
-    console.log(result.body);
-
-    return true;
+    expect(result.body).toEqual(todo);
   });
 });
--- test/utils/test.utils.ts
+++ test/utils/test.utils.ts
@@ -2,6 +2,10 @@ import { Prisma, Todo } from "@prisma/client";
 import { INestApplication } from "@nestjs/common";
 import { PrismaService } from "@app/prisma/prisma.service";
 
+interface WithId {
+    id: BigInt | number;
+}
+
 export class TestUtils {
     private prismaService: PrismaService;
     private app: INestApplication;
@@ -11,9 +15,15 @@ export class TestUtils {
         this.prismaService = app.get<PrismaService>(PrismaService);
     }
 
+    formatObject<T extends WithId>(testObject: T): T {
+        return { ...testObject, id: Number(testObject.id) }
+    }
+
     async createTodo(
         todo: Prisma.TodoCreateInput,
     ): Promise<Todo> {
-        return this.prismaService.todo.create({ data: todo });
+        const createdTodo = await this.prismaService.todo.create({ data: todo });
+
+        return this.formatObject(createdTodo);
     }
 }
